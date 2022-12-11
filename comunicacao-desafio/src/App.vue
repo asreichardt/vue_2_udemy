16c9f36d13666582b21280283196b93dfc81ac88
asreichardt <65123372+asreichardt@users.noreply.github.com>
2022-12-11 12:23:55 -0300

    p

---
+++ comunicacao-desafio/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+	<div id="app" >
+		<p><strong> {{usuarioSelecionado.nome }}</strong></p> 
+		<p><strong> {{usuarioSelecionado.id }}</strong></p> 
+		<usuario-lista :usuarios="usuarios" @usuarioMudou="usuarioSelecionado = $event"	/>
+		
+		<usuario-detalhe  :id="usuarioSelecionado.id" :nome="usuarioSelecionado.nome"    />
+	</div>
+</template>
+
+<script>
+import UsuarioLista from './components/UsuarioLista.vue'
+import UsuarioDetalhe from './components/UsuarioDetalhe.vue'
+
+export default {
+	name: "app",
+	components: { UsuarioLista, UsuarioDetalhe },
+    data() {
+        return {
+            usuarios: [
+                { id: 1, nome: 'Ana', idade: 31 },
+                { id: 2, nome: 'Carlos', idade: 22 },
+                { id: 3, nome: 'Daniel', idade: 40 },
+                { id: 4, nome: 'Ema', idade: 17 },
+                { id: 5, nome: 'Lia', idade: 28 }
+            ],
+			usuarioSelecionado: { id:'', nome: ''}
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+
+	display: block;
+	font-size: 2rem;
+}
+
+#app div {
+	margin: 20px;
+}
+</style>
---
+++ comunicacao-desafio/src/components/UsuarioDetalhe.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="usuario-detalhe">
+        <h2>Nome: {{ this.nome }}</h2>
+        <h2>Id: {{ this.id }}</h2>     
+    </div>
+</template>
+
+<script>
+export default {
+      props : {
+        id: Number,
+        nome: String
+      }
+}
+</script>
+<style>
+    .usuario-detalhe {
+        flex: 1;
+        border: 1px solid #CCC;
+    }
+</style>
